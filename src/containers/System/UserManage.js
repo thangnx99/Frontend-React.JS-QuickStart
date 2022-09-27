52debf24c179269005949d89a739a4bc94bb616f
thangnx99 <thang999nx@gmail.com>
2022-09-27 17:28:40 +0700

    login5

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers } from '../../services/userService'
+import { first } from 'lodash';
 class UserManage extends Component {
 
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
 
+        }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
 
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className='title text-center'>Manage users with react</div>
+                <div className='users-table mt-3 mx-1'>
+
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+
+                        {arrUsers && arrUsers.map((item, index) => {
+
+                            return (
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <button className='btn-edit'><i class="fas fa-pencil-alt"></i></button>
+                                    <button className='btn-delete'><i class="fas fa-trash"></i></button>
+                                </tr>
+                            )
+                        })
+
+                        }
+
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,5 +4,8 @@ const handleLoginApi = (email, password) => {
     return axios.post('/api/login', { email, password });
 
 }
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
 
-export { handleLoginApi }
+export { handleLoginApi, getAllUsers }
