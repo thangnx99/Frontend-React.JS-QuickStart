8a027f598837b6e078eb4ab0f74b811ce4e03760
thangnx99 <thang999nx@gmail.com>
2022-09-27 23:16:12 +0700

    login7

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
 import { getAllUsers } from '../../services/userService'
-import { first } from 'lodash';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
 
         }
     }
@@ -24,12 +26,33 @@ class UserManage extends Component {
 
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
 
+    }
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+
+    }
     render() {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="user-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                />
                 <div className='title text-center'>Manage users with react</div>
+                <div className='mx-1'>
+                    <button
+                        className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewUser()}
+                    ><i class="fas fa-plus"></i>Add new users</button>
+                </div>
                 <div className='users-table mt-3 mx-1'>
 
                     <table id="customers">
@@ -44,7 +67,7 @@ class UserManage extends Component {
                         {arrUsers && arrUsers.map((item, index) => {
 
                             return (
-                                <tr>
+                                <tr key={index}>
                                     <td>{item.email}</td>
                                     <td>{item.firstName}</td>
                                     <td>{item.lastName}</td>
